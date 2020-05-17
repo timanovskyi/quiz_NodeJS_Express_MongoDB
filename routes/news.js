6c1384b2e3d10b47a08ada98805123d75cb434dc
Egor <ytimanovskyi@betacom.com.pl>
2020-05-17 18:54:25 +0200

    updated news page

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const News = require('../models/news')
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('news', { title: 'News' });
+const search = req.query.search || '';
+
+
+  const findNews = News
+  .find({title: new RegExp(search.trim(), 'i')  })
+  .sort({created: -1});
+
+  findNews.exec((err, data) => {
+    res.render('news', { title: 'News', data, search });
+  });
 });
 
 
